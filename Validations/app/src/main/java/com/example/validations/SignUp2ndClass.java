abd9a7686557083ce2845352a91415bbd90ece12
Adnan112-dev <79618300+Adnan112-dev@users.noreply.github.com>
2021-11-18 21:03:14 +0530

    Final APP

---
+++ Validations/app/src/main/java/com/example/validations/SignUp2ndClass.java
@@ -0,0 +1,77 @@
+package com.example.validations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class SignUp2ndClass extends AppCompatActivity {
+
+
+//    Variables
+
+    Button registerBtn;
+
+
+    RadioGroup radioGroup;
+    RadioButton selectedAge;
+    DatePicker datePicker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2nd_class);
+
+
+        //        Hooks
+        registerBtn = findViewById(R.id.register_button);
+        radioGroup = findViewById(R.id.radio_group);
+        datePicker = findViewById(R.id.age_picker);
+    }
+
+
+    public void callRegister(View view) {
+        if(!validateGender() | !validateAge() ){
+            return;
+        }else{
+            Toast.makeText(this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private boolean validateGender() {
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    private boolean validateAge() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int userAge = datePicker.getYear();
+
+        int isAgeValid = currentYear - userAge;
+
+
+        if (isAgeValid < 13) {
+            Toast.makeText(this, "You are not Eligible to Apply Minimum Age is 13", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+}
